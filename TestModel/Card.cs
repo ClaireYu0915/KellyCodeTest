327c93beb1d92d423599ffa7b2e2fa70cad7d244
ClaireYu0915 <claireyu0915@gmail.com>
2017-06-30 18:36:14 -0400

    Add files via upload

---
+++ TestModel/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModel
+{
+    public class Card
+    {
+        public int CardID { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
